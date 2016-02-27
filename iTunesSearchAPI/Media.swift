69b2ccd53c9064c2587b6c1f940b5b52659b5336
Sven Bacia <sven.bacia@gmail.com>
2016-02-27 23:22:39 +0100

    adds media

---
+++ iTunesSearchAPI/Media.swift
@@ -0,0 +1,22 @@
+//
+//  Media.swift
+//  iTunesSearchAPI
+//
+//  Created by Sven Bacia on 27.02.16.
+//  Copyright © 2016 Sven Bacia. All rights reserved.
+//
+
+import Foundation
+
+public enum Media: String {
+  case Movie      = "movie"
+  case Podcast    = "podcast"
+  case Music      = "music"
+  case MusicVideo = "musicVideo"
+  case AudioBook  = "audiobook"
+  case ShortFilm  = "shortFilm"
+  case TVShow     = "tvShow"
+  case Software   = "software"
+  case eBook      = "ebook"
+  case All        = "all"
+}
