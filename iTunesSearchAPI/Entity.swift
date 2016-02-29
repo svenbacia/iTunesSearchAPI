bc2c20a9c02280495a17a71154ded7d9e606175c
Sven Bacia <sven.bacia@gmail.com>
2016-02-29 18:37:53 +0100

    [WIP] improves search query

--- iTunesSearchAPI/Entity.swift
+++ iTunesSearchAPI/Entity.swift
@@ -8,64 +8,10 @@
 
 import Foundation
 
-public struct Entity: OptionSetType {
-  
-  // MARK: - Private Extended Type
-  
-  private enum EntityType: Int, CustomStringConvertible {
-    case TVEpisode = 1
-    case TVSeason  = 2
-    
-    static var allTypes: [EntityType] {
-      return [.TVEpisode, .TVSeason]
-    }
-    
-    var description: String {
-      switch self {
-      case .TVEpisode:
-        return "tvEpisode"
-      case .TVSeason:
-          return "tvSeason"
-      }
-    }
-  }
-  
-  // MARK: - Options
-  
-  /// Default Returns enough info to match locally.
-  public static let TVEpisode = Entity(.TVEpisode)
-  
-  /// Minimal info and all images.
-  public static let TVSeason = Entity(.TVSeason)
-  
-  // MARK: - Properties
-  
-  public let rawValue: Int
-  
-  // MARK: - Init
-  
-  public init(rawValue: Int) {
-    self.rawValue = rawValue
-  }
-  
-  private init(_ entityType: EntityType) {
-    self.rawValue = entityType.rawValue
-  }
-  
-  // MARK: - CustomStringConvertible
-  
-  public var description: String {
-    return EntityType.allTypes
-      .filter { self.contains(Entity($0)) }
-      .reduce("") { memo, currentType in
-        memo == "" ? "\(currentType)" : "\(memo),\(currentType)"
-    }
-  }
-}
+public protocol Entity: CustomStringConvertible, QueryItemConvertible {}
 
 extension Entity {
-  /// Converts the extended information into an array of query items.
-  var queryItem: NSURLQueryItem {
-    return NSURLQueryItem(name: "entity", value: description)
+  public var queryItems: [NSURLQueryItem] {
+    return [NSURLQueryItem(name: "entity", value: description)]
   }
 }
--- iTunesSearchAPI/Media.swift
+++ iTunesSearchAPI/Media.swift
@@ -9,15 +9,89 @@
 import Foundation
 
 /// The media type you want to search for. The default is all.
-public enum Media: String {
-  case Movie      = "movie"
-  case Podcast    = "podcast"
-  case Music      = "music"
-  case MusicVideo = "musicVideo"
-  case AudioBook  = "audiobook"
-  case ShortFilm  = "shortFilm"
-  case TVShow     = "tvShow"
-  case Software   = "software"
-  case eBook      = "ebook"
-  case All        = "all"
+public enum Media {
+  case Movie(String, includeEntities: Entity?)
+  case Podcast(String, includeEntities: Entity?)
+  case Music(String, includeEntities: Entity?)
+  case MusicVideo(String, includeEntities: Entity?)
+  case AudioBook(String, includeEntities: Entity?)
+  case ShortFilm(String, includeEntities: Entity?)
+  case TVShow(String, includeEntities: TVShowEntity?)
+  case Software(String, includeEntities: Entity?)
+  case eBook(String, includeEntities: Entity?)
+  case Everything(String, includeEntities: Entity?)
+  
+  private var value: String {
+    switch self {
+    case .Movie:
+      return "movie"
+    case .Podcast:
+      return "podcast"
+    case .Music:
+      return "music"
+    case .MusicVideo:
+      return "musicvideo"
+    case .AudioBook:
+      return "audiobook"
+    case .ShortFilm:
+      return "shortFilm"
+    case .TVShow:
+      return "tvShow"
+    case .Software:
+      return "software"
+    case .eBook:
+      return "ebook"
+    case .Everything:
+      return "all"
+    }
+  }
+}
+
+extension Media: QueryItemConvertible {
+  public var queryItems: [NSURLQueryItem] {
+    switch self {
+    case .Movie(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .Podcast(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .Music(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .MusicVideo(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .AudioBook(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .ShortFilm(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .TVShow(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .Software(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .eBook(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    case .Everything(let title, let entities):
+      return queryItemsFor(title, includedEntities: entities)
+    }
+  }
+  
+  private func queryItemsFor(title: String, includedEntities entities: Entity?) -> [NSURLQueryItem] {
+    // make sure that the title is valid to use in an url
+    guard let escapedTitle = title.URLEscapedString else { return [] }
+    
+    // create query item for the search term
+    let termQueryItem = NSURLQueryItem(name: "term", value: escapedTitle)
+    
+    // create query item for selected media type
+    let mediaQueryItem = NSURLQueryItem(name: "media", value: value)
+    
+    // add term and media query item to one array
+    let requiredQueryItems = [termQueryItem, mediaQueryItem]
+    
+    // create query items for possible entities when needed
+    if let entityQueryItems = entities?.queryItems {
+      return requiredQueryItems + entityQueryItems
+    }
+    
+    // otherwise return only the term
+    return requiredQueryItems
+  }
 }
