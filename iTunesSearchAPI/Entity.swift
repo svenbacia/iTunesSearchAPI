b183dad283b5d866ed9ac53fba89770813296534
Sven Bacia <sven.bacia@gmail.com>
2016-04-16 22:08:40 +0200

    updates entity

--- iTunesSearchAPI/Entity.swift
+++ iTunesSearchAPI/Entity.swift
@@ -2,16 +2,104 @@
 //  Entity.swift
 //  iTunesSearchAPI
 //
-//  Created by Sven Bacia on 28.02.16.
+//  Created by Sven Bacia on 16.04.16.
 //  Copyright © 2016 Sven Bacia. All rights reserved.
 //
 
 import Foundation
 
-public protocol Entity: CustomStringConvertible, QueryItemConvertible {}
+/// The type of results you want returned, relative to the specified media type.
+/// For example: movieArtist for a movie media type search.
+/// The default is the track entity associated with the specified media type.
+public enum Entity {
+  // All
+  case Movie
+  case Album
+  case AllArtist
+  case Podcast
+  case MusicVideo
+  case Mix
+  case Audiobook
+  case TVSeason
+  case AllTrack
+  
+  // Movie
+  case MovieArtist
+  
+  // Podcast
+  case PodcastAuthor
+  
+  // Music
+  case MusicArtist
+  case MusicTrack
+  case MusicSong
+  
+  // Audiobook
+  case AudiobookAuthor
+  
+  // Shortfilm
+  case ShortFilmArtist
+  case ShortFilm
+  
+  // TVShow
+  case TVEpisode
+  
+  // Software
+  case Software
+  case iPadSoftware
+  case MacSoftware
+  
+  // eBook
+  case eBook
+}
 
-extension Entity {
-  public var queryItems: [NSURLQueryItem] {
-    return [NSURLQueryItem(name: "entity", value: description)]
+extension Entity: EntityType {
+  public var value: String {
+    switch self {
+    case .Movie:
+      return "movie"
+    case .Album:
+      return "album"
+    case .AllArtist:
+      return "allArtist"
+    case .Podcast:
+      return "podcast"
+    case .MusicVideo:
+      return "musicVideo"
+    case .Mix:
+      return "mix"
+    case .Audiobook:
+      return "audiobook"
+    case .TVSeason:
+      return "tvSeason"
+    case .AllTrack:
+      return "allTrack"
+    case .MovieArtist:
+      return "movieArtist"
+    case .PodcastAuthor:
+      return "podcastAuthor"
+    case .MusicArtist:
+      return "musicArtist"
+    case .MusicTrack:
+      return "musicTrack"
+    case .MusicSong:
+      return "musicSong"
+    case .AudiobookAuthor:
+      return "audiobookAuthor"
+    case .ShortFilmArtist:
+      return "shortFilmArtist"
+    case .ShortFilm:
+      return "shortFilm"
+    case .TVEpisode:
+      return "tvEpisode"
+    case .Software:
+      return "software"
+    case .iPadSoftware:
+      return "iPadSoftware"
+    case .MacSoftware:
+      return "macSoftware"
+    case .eBook:
+      return "ebook"
+    }
   }
 }
