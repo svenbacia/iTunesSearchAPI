99266f4e7055a8437a6ece6fa91df57035341355
Sven Bacia <sven.bacia@gmail.com>
2016-04-16 19:03:36 +0200

    adds Dictionary extension

---
+++ iTunesSearchAPI/Dictionary+Merge.swift
@@ -0,0 +1,17 @@
+//
+//  Dictionary+Merge.swift
+//  iTunesSearchAPI
+//
+//  Created by Sven Bacia on 16.04.16.
+//  Copyright © 2016 Sven Bacia. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+  mutating func unionInPlace(dictionary: Dictionary<Key, Value>) {
+    for (key, value) in dictionary {
+      self[key] = value
+    }
+  }
+}
