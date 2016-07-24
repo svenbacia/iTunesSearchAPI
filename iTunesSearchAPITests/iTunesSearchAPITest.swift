2f4ed5f7fbc22946771053f583d33539ce5b5472
Sven Bacia <sven.bacia@gmail.com>
2016-07-24 19:40:18 +0200

    removes quick / nimble

---
+++ iTunesSearchAPITests/iTunesSearchAPITest.swift
@@ -0,0 +1,34 @@
+//
+//  iTunesSearchAPITest.swift
+//  iTunesSearchAPI
+//
+//  Created by Sven Bacia on 24.07.16.
+//  Copyright © 2016 Sven Bacia. All rights reserved.
+//
+
+import XCTest
+
+@testable import iTunesSearchAPI
+
+class iTunesSearchAPITest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+  
+  func testSingleton_notNil() {
+  }
+
+    
+}
