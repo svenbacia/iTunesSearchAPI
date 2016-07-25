91479e22c5c05c0967aa751f220e574bdaa15fd2
Sven Bacia <sven.bacia@gmail.com>
2016-07-25 09:33:28 +0200

    adds language tests

---
+++ iTunesSearchAPITests/LanguageTests.swift
@@ -0,0 +1,20 @@
+//
+//  LanguageTests.swift
+//  iTunesSearchAPI
+//
+//  Created by Sven Bacia on 25.07.16.
+//  Copyright © 2016 Sven Bacia. All rights reserved.
+//
+
+import XCTest
+
+@testable import iTunesSearchAPI
+
+class LanguageTests: XCTestCase {
+  
+  func testLanguageCodes() {
+    XCTAssertEqual(Language.english.rawValue, "en_us")
+    XCTAssertEqual(Language.japanese.rawValue, "ja_jp")
+  }
+  
+}
